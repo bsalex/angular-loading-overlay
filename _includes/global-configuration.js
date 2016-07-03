ebe7911778ee02a32af563d3690b137a27867e76
Alexander Beshchuk <bs.alex.mail@gmail.com>
2016-07-04 00:04:10 +0300

    More docs for templateOptions. Docs style consistency inprovements.

--- _includes/global-configuration.js
+++ _includes/global-configuration.js
@@ -5,5 +5,6 @@ angular.module('app', [
     delay: 0, // Minimal delay to hide loading overlay in ms.
     activeClass: undefined, // Class that is added to the element where bs-loading-overlay is applied when the overlay is active.
     templateUrl: undefined // Template url for overlay element. If not specified - no overlay element is created.
+    templateOptions: undefined // Options that are passed to overlay template (specified by templateUrl option above).
   });
 });
