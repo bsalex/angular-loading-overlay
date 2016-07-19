6157379357015b633d6edd9cbffd9dfedab4adf4
Alexander Beshchuk <bs.alex.mail@gmail.com>
2016-07-19 23:41:00 +0300

    Interceptor with matcher example simpllification

--- _includes/examples/http-integration-with-reference-id-and-matchers/http-integration-with-reference-id-and-matchers.controller.js
+++ _includes/examples/http-integration-with-reference-id-and-matchers/http-integration-with-reference-id-and-matchers.controller.js
@@ -1,22 +1,21 @@
 app.controller('{{include.controller_name}}', function($scope, $http, $sce, bsLoadingOverlayService) {
-    $scope.vm = vm = {};
-    vm.randomText = $sce.trustAsHtml('Fetch result here');
-    vm.randomUser = {};
+    $scope.randomText = $sce.trustAsHtml('Fetch result here');
+    $scope.randomUser = {};
 
     $scope.fetchRandomText = function() {
         $http.get('http://hipsterjesus.com/api/')
             .success(function(data) {
-                vm.randomText = $sce.trustAsHtml(data.text);
+                $scope.randomText = $sce.trustAsHtml(data.text);
             })
             .error(function() {
-                vm.randomText = $sce.trustAsHtml('Can not get the article');
+                $scope.randomText = $sce.trustAsHtml('Can not get the article');
             });
     };
 
     $scope.fetchRandomUser = function() {
         $http.get('https://randomuser.me/api/')
             .success(function(data) {
-                vm.randomUser = data.results[0];
+                $scope.randomUser = data.results[0];
             });
     };
 });
